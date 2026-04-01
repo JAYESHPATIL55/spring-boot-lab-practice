dc800eae5009d7d0673a3ceb0a40f86900b0ca2e
JAYESHPATIL55 <jayeshpatil8744@gmail.com>
2026-04-01 21:09:26 +0530

    Day13 Exam On Crud Operation

---
+++ Day13-Exam-On-Crud-Operation-And-Layerd-Application/Employee-CRUD-Exam/src/main/java/com/nt/model/Employee.java
@@ -0,0 +1,14 @@
+package com.nt.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+@Data
+public class Employee {
+	private int eId;
+	private String eName;
+	private String eDepartment;
+	private double eSalary;
+	
+	
+}
