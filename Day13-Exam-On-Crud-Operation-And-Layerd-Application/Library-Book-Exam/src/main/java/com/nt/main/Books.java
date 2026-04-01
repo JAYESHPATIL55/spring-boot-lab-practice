7a12182730256cb810e2bc483854c33547938b41
JAYESHPATIL55 <jayeshpatil8744@gmail.com>
2026-04-01 21:14:08 +0530

    Third Question On CRUD and Layerd Application

---
+++ Day13-Exam-On-Crud-Operation-And-Layerd-Application/Library-Book-Exam/src/main/java/com/nt/main/Books.java
@@ -0,0 +1,11 @@
+package com.nt.main;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+public class Books {
+	private String name;
+}
