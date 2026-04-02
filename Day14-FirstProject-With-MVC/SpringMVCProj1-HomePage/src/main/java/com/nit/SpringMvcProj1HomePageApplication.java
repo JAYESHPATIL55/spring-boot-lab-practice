7d4cd701f031f68879aedbd411b7d3d84f5e3d58
JAYESHPATIL55 <jayeshpatil8744@gmail.com>
2026-04-02 21:32:37 +0530

    More Practicing the Spring Boot MVC

---
+++ Day14-FirstProject-With-MVC/SpringMVCProj1-HomePage/src/main/java/com/nit/SpringMvcProj1HomePageApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcProj1HomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcProj1HomePageApplication.class, args);
+	}
+
+}
