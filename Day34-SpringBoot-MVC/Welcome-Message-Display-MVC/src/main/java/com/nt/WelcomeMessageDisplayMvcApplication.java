bbf0d8cae235b52065b5127987b34a0375922fb3
JAYESHPATIL55 <jayeshpatil8744@gmail.com>
2026-05-05 22:51:05 +0530

    Day 34 Spring Boot MVC Starting

---
+++ Day34-SpringBoot-MVC/Welcome-Message-Display-MVC/src/main/java/com/nt/WelcomeMessageDisplayMvcApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeMessageDisplayMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeMessageDisplayMvcApplication.class, args);
+	}
+
+}
